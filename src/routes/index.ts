e8cc10243957ff662f4a02bfb58c2a8d86b99140
Jonatas Assis <jonatasassis@Jonatass-MacBook-Pro.local>
2020-10-28 17:14:44 -0300

    Criando a conexão com o Banco de Dados

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express';
+import appointmentsRoutes from './appointments.routes';
 
 const routes = Router();
+
+routes.get('appointments', appointmentsRoutes);
+routes.post('appointments', appointmentsRoutes);
 
 export default routes;
