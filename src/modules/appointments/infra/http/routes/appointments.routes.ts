61d4783e8bd7b661dbce8f9b2186b0063246f6d2
Jonatas Assis <jonatasassis@Jonatass-MacBook-Pro.local>
2021-02-01 22:25:52 -0300

    Aplicando principio do Solid chamado Liskov

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,7 +2,7 @@ import { parseISO } from 'date-fns';
 import { Router } from 'express';
 import { getCustomRepository } from 'typeorm';
 import ensureAuthenticated from '@shared/middlewares/ensureAuthenticated';
-import AppointmentsRepository from '@modules/appointments/repositories/AppointmentsRepository';
+import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 const appointmentsRoutes = Router();
