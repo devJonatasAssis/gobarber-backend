ccfd2040e098c1b5ebca00fd01a154760f1035c0
Jonatas de Assis Silva <jonatassilva9090@gmail.com>
2021-03-22 23:16:53 -0300

    Criando testes unitários para reset de senha e envio de email

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,4 +1,5 @@
 import { container } from 'tsyringe';
+import IMailProvider from './MailProvider/models/IMailProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 
